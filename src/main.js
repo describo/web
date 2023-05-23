98e4d85b883d34d616e258a5afd01f90a346d4a1
Marco La Rosa <m@lr.id.au>
2023-05-23 20:13:24 +1000

    import dep's, revise app startup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import "./tailwind.css";
+import "@element-plus/theme-chalk/dist/index.css";
+import "@fortawesome/fontawesome-free/js/all";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoReplaceSvg = "nest";
+
 import { createApp } from "vue";
 import DescriboCrateBuilder from "@describo/crate-builder-component";
+// import DescriboCrateBuilder from "../../crate-builder-component/src/crate-builder/index.js";
 import App from "./App.vue";
 import { router } from "./routes.js";
 import { store } from "./store.js";
@@ -8,7 +14,5 @@ import { store } from "./store.js";
 const app = createApp(App);
 app.use(router);
 app.use(store);
-// app.use(DescriboCrateBuilder);
-router.isReady().then(() => {
-    app.mount("#app");
-});
+app.use(DescriboCrateBuilder);
+app.mount("#app");
