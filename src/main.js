c5191410172bc7c6b5464a9910994e629328e2e1
Marco La Rosa <m@lr.id.au>
2023-06-07 16:10:50 +1000

    use the component from the npm package

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoReplaceSvg = "nest";
 
 import { createApp } from "vue";
-// import DescriboCrateBuilder from "@describo/crate-builder-component";
-import DescriboCrateBuilder from "../../crate-builder-component/src/crate-builder/index.js";
+import DescriboCrateBuilder from "@describo/crate-builder-component";
+// import DescriboCrateBuilder from "../../crate-builder-component/src/crate-builder/index.js";
 import App from "./App.vue";
 import { router } from "./routes.js";
 import { store } from "./store.js";
