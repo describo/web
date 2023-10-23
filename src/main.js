3f7c66298ac52afc2b566b581474b84f74ee2497
Marco La Rosa <m@lr.id.au>
2023-10-23 13:19:39 +1100

    update dependencies and revise app setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import "./tailwind.css";
-import "@element-plus/theme-chalk/dist/index.css";
+import "element-plus/dist/index.css";
 import "@fortawesome/fontawesome-free/js/all";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoReplaceSvg = "nest";
 
 import { createApp } from "vue";
+import ElementPlus from "element-plus";
 import DescriboCrateBuilder from "@describo/crate-builder-component";
 // import DescriboCrateBuilder from "../../crate-builder-component/src/crate-builder/index.js";
 import App from "./App.vue";
@@ -15,6 +16,8 @@ const app = createApp(App);
 app.use(router);
 app.use(store);
 router.isReady().then(() => {
+    app.use(ElementPlus);
+
     app.use(DescriboCrateBuilder);
     app.mount("#app");
 });
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 export default defineConfig({
     base: process.env.NODE_ENV === "development" ? "/" : "/web",
-    plugins: [
-        vue(),
-        // ...
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-    ],
+    plugins: [vue()],
     optimizeDeps: {
-        include: ["element-plus", "lodash", "@describo/crate-builder-component", "ajv"],
+        include: ["element-plus", "@describo/crate-builder-component", "ajv"],
     },
     build: {
         outDir: "docs",
